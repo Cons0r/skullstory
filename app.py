b1ff8dda59fdb8a8b9a2eb0b7d2d74d5e0fdb376
MaverickQuill55 (VS Code) <VScode@toxik.cf>
2021-10-26 14:16:38 -0400

    added h1

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
 from website import create_app
-app = create_app()
-
 if __name__ == "__main__":
-  app.run('0.0.0.0', 8080, True)
+  create_app().run('0.0.0.0', 8080, True)
--- website/__init__.py
+++ website/__init__.py
@@ -2,12 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
-
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "helloworld"
-
+    
     from .views import views
     from .auth import auth
 
