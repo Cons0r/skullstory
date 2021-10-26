a76c180cfdac72e9f08ad11774aed8bff9bf9347
MaverickQuill55 (VS Code) <VScode@toxik.cf>
2021-10-26 12:04:54 -0400

    Added and setup website module

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
 from website import create_app
 app = create_app()
-=======
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-  return "Hello again World!"
->>>>>>> a76924e01e7b11824210b7f45cb4c0eb17ce6020
 
 if __name__ == "__main__":
   app.run('0.0.0.0', 8080, True)
