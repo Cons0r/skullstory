3fddcc13ec44e0248e16b763f38cc33b74278e71
maverickquill55 <VScode@toxik.cf>
2021-11-02 10:58:44 -0400

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -22,7 +22,6 @@ class User(db.Model, UserMixin):
   email = db.Column(db.String(150), unique=True)
   username = db.Column(db.String(150), unique=True)
   password = db.Column(db.String(150), unique=False)
-  dev = db.Column(db.Boolean)
   date_created = db.Column(db.DateTime(timezone=True), default=apis.now())
   posts = db.relationship('Post', backref='user', passive_deletes=True)
   gravatar = db.Column(db.String(150), unique=True)
